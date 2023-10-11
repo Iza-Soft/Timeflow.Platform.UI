3fcbc373f371dd7917e2a71855079c95a3f93f26
DESKTOP-KJNO7KJ\Ilko Adamov <ilko.z.adamov@gmail.com>
2023-10-11 15:02:59 +0300

    Create UI structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { IconSvgComponent } from './shared/icon-svg/icon-svg.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, IconSvgComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'iza-header',
+  selector: 'tf-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public showFilledHomeIcon: boolean = true;
+  public showFilledProfileIcon: boolean = true;
+
+  constructor() {}
+
+  public onMouseEnter(icon: string) {
+    if (icon.includes('home')) this.showFilledHomeIcon = false;
+    if (icon.includes('profile')) this.showFilledProfileIcon = false;
+  }
+
+  public onMouseLeave(icon: string) {
+    if (icon.includes('home')) this.showFilledHomeIcon = true;
+    if (icon.includes('profile')) this.showFilledProfileIcon = true;
+  }
+
+  public onTimeSheet() {
+    console.log('show timesheet');
+  }
+
+  public onProfile() {
+    console.log('open dropdown menu');
+  }
+
+  public onProject() {
+    console.log('show all projects');
+  }
+
+  public onDashboard() {
+    console.log('go to dashboard');
+  }
+}
