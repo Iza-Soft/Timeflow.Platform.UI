aa0f9d7d7696c3a4a98997be65e2b5bbc093dacc
DESKTOP-KJNO7KJ\Ilko Adamov <ilko.z.adamov@gmail.com>
2023-11-14 13:43:12 +0200

    Added interceptors about jwt, error, http service

--- src/app/services/interceptors/jwt.interceptor.ts
+++ src/app/services/interceptors/jwt.interceptor.ts
@@ -17,16 +17,19 @@ export class JwtInterceptor implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    sessionStorage.setItem(
-      'access-token',
-      'aeyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiaWxrby56LmFkYW1vdkBnbWFpbC5jb20iLCJqdGkiOiJlZWE2OWRhNy00ZTllLTRjYzUtOGVhMy1jZmQ3NDNhMzhkYzUiLCJleHAiOjE2OTk4ODE3NTgsImlzcyI6IklaQVNvZnQuY29tIiwiYXVkIjoiSVpBU29mdC5jb20ifQ.AS-HK_qMHUov0qR0mUzq8YS4-Egpn7PnjVGEH5Ro27pHJXK_8W8J_eJXsriDZONDmCh-MVbr9O5PpOahN7-myg'
-    );
+    // sessionStorage.setItem(
+    //   'access-token',
+    //   'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiaWxrby56LmFkYW1vdkBnbWFpbC5jb20iLCJqdGkiOiJjMjUzYmE2NC1hMzkxLTRhOTctYmJiNC1jZDBhMWU5ZmIyNWYiLCJleHAiOjE2OTk5MjYyMzksImlzcyI6IklaQVNvZnQuY29tIiwiYXVkIjoiSVpBU29mdC5jb20ifQ.bD9XU-R_73uGNXAx0dwbKGnNiyAze5dxxHGWUlpXQ4wvEjM5DAcR2oMSddTF82jlz4zeO97q2bJhnWOudHqW5g'
+    // );
     const token = sessionStorage.getItem('access-token');
 
     if (token) {
       const modifiedRequest = request.clone({
         setHeaders: {
           Authorization: 'Bearer ' + token,
+          // 'Access-Control-Allow-Origin': 'http://timeflow.iza-soft.com/',
+          // 'Access-Control-Allow-Headers':
+          //   'Origin, X-Requested-With, Content-Type, Accept, Authorization',
         },
       });
       return next.handle(modifiedRequest);
