2b12c713dd68a411e050dfed846f8eaf54ddf8f9
DESKTOP-KJNO7KJ\Ilko Adamov <ilko.z.adamov@gmail.com>
2023-11-20 16:54:00 +0200

    Created simple and searchable dropdown

--- src/app/shared/components/select/select.component.ts
+++ src/app/shared/components/select/select.component.ts
@@ -9,52 +9,74 @@ import { ISelectModel } from '../../models/select/select-model';
 })
 export class SelectComponent implements OnInit {
   @Input() dataSource: ISelectModel[];
+  @Input() readonly: boolean;
+  @Input() placeholder: string;
+  @Input() value: { id: number; parentId?: number };
   @Output() onSelectItem = new EventEmitter<ISelectModel[]>();
 
   public title?: string;
   public _dataSource: ISelectModel[];
+  public notSimpleMenu: boolean;
   constructor() {}
 
   public ngOnInit(): void {
     this._dataSource = this.CopyDataSource();
+
+    this.notSimpleMenu = this._dataSource.some(
+      (item) => item.Collection?.length !== 0
+    );
+
+    if (this.value) {
+      this.onSelectOption(this.value.id, this.value.parentId);
+    }
   }
 
   public onSelectOption(
-    dropdown: BsDropdownDirective,
     id: number,
-    parentId?: number
+    parentId?: number,
+    dropdown?: BsDropdownDirective
   ) {
     this._dataSource = this.CopyDataSource();
-
-    this.title = this._dataSource
-      .find((item) => item.Id === parentId)
-      ?.Collection?.find((item) => item.Id === id)?.Title;
-
     let selectedItem: ISelectModel[] | undefined = undefined;
-    this._dataSource.map((parent) => {
-      let findItem = parent.Collection?.find(
-        (item) => item.Id === id && item.ParentId === parentId
-      );
-      if (findItem) {
-        selectedItem = [
-          {
-            Id: parent.Id,
-            Title: parent.Title,
-            Description: parent.Description,
-            Collection: [
-              {
-                Id: findItem.Id,
-                ParentId: findItem.ParentId,
-                Title: findItem.Title,
-                Description: findItem.Description,
-              },
-            ],
-          },
-        ];
-      }
-    });
-    if (dropdown.isOpen) {
-      dropdown.hide();
+    if (parentId) {
+      this.title = this._dataSource
+        .find((item) => item.Id === parentId)
+        ?.Collection?.find((item) => item.Id === id)?.Title;
+
+      this._dataSource.map((parent) => {
+        let findItem = parent.Collection?.find(
+          (item) => item.Id === id && item.ParentId === parentId
+        );
+        if (findItem) {
+          selectedItem = [
+            {
+              Id: parent.Id,
+              Title: parent.Title,
+              Description: parent.Description,
+              Collection: [
+                {
+                  Id: findItem.Id,
+                  ParentId: findItem.ParentId,
+                  Title: findItem.Title,
+                  Description: findItem.Description,
+                },
+              ],
+            },
+          ];
+        }
+      });
+    } else {
+      let findItem = this._dataSource.find((item) => item.Id === id);
+      this.title = findItem?.Title;
+      selectedItem = [
+        {
+          Id: findItem!.Id,
+          Title: findItem!.Title,
+        },
+      ];
+    }
+    if (dropdown?.isOpen) {
+      dropdown?.hide();
     }
     this.onSelectItem.emit(selectedItem);
   }
